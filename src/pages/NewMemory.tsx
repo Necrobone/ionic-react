36994583c80fdf65b0fcd3dad4949fe55a7efa8a
Necrobone <necrobone@hotmail.com>
2022-02-25 17:44:59 +0100

    New memory page

--- src/pages/NewMemory.tsx
+++ src/pages/NewMemory.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import {
   IonBackButton,
+  IonButton,
   IonButtons,
+  IonCol,
   IonContent,
+  IonGrid,
   IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonLabel,
   IonPage,
+  IonRow,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
+import { camera } from "ionicons/icons";
+
+import "./NewMemory.css";
 
 const NewMemory: React.FC = () => {
   return (
@@ -21,7 +32,32 @@ const NewMemory: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <h2>New Memory Page</h2>
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating">Memory Title</IonLabel>
+                <IonInput type="text" />
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow className="ion-text-center">
+            <IonCol>
+              <div className="image-preview">
+                <h3>No photo chosen.</h3>
+              </div>
+              <IonButton fill="clear">
+                <IonIcon icon={camera} slot="start" />
+                <IonLabel>Take Photo</IonLabel>
+              </IonButton>
+            </IonCol>
+          </IonRow>
+          <IonRow className="ion-margin-top">
+            <IonCol className="ion-text-center">
+              <IonButton>Add Memory</IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
