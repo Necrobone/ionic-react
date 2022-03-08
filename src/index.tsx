b992ba093d3f09beb4bccb1d7750f6696ed9d6c0
Necrobone <necrobone@hotmail.com>
2022-03-08 18:46:19 +0100

    Preparing files to deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
+import { defineCustomElements } from "@ionic/pwa-elements/loader";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import MemoriesContextProvider from "./data/MemoriesContextProvider";
-import { defineCustomElements } from "@ionic/pwa-elements/loader";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,3 +15,8 @@ ReactDOM.render(
 );
 
 defineCustomElements(window);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
